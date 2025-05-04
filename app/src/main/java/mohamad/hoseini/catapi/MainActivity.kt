29377189128daf1073c9998726609e3409bd0995
Mohamad Hoseini <mhoseini967@gmail.com>
2025-05-04 13:08:41 +0330

    add basic colors for dark/light color

--- app/src/main/java/mohamad/hoseini/catapi/MainActivity.kt
+++ app/src/main/java/mohamad/hoseini/catapi/MainActivity.kt
@@ -4,8 +4,10 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -16,10 +18,11 @@ import mohamad.hoseini.catapi.ui.theme.CatAPITheme
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
+
+//        Todo read dark theme from shared preferences
         setContent {
-            CatAPITheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+            CatAPITheme(darkTheme = false) {
+                Scaffold(modifier = Modifier.fillMaxSize().background(MaterialTheme.colorScheme.background)) { innerPadding ->
                     Greeting(
                         name = "Android",
                         modifier = Modifier.padding(innerPadding)
--- app/src/main/java/mohamad/hoseini/catapi/ui/theme/Theme.kt
+++ app/src/main/java/mohamad/hoseini/catapi/ui/theme/Theme.kt
@@ -1,53 +1,57 @@
 package mohamad.hoseini.catapi.ui.theme
 
-import android.app.Activity
-import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.darkColorScheme
-import androidx.compose.material3.dynamicDarkColorScheme
-import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.colorResource
+import com.google.accompanist.systemuicontroller.rememberSystemUiController
+import mohamad.hoseini.catapi.R
 
-private val DarkColorScheme = darkColorScheme(
-    primary = Purple80,
-    secondary = PurpleGrey80,
-    tertiary = Pink80
-)
-
-private val LightColorScheme = lightColorScheme(
-    primary = Purple40,
-    secondary = PurpleGrey40,
-    tertiary = Pink40
-
-    /* Other default colors to override
-    background = Color(0xFFFFFBFE),
-    surface = Color(0xFFFFFBFE),
-    onPrimary = Color.White,
-    onSecondary = Color.White,
-    onTertiary = Color.White,
-    onBackground = Color(0xFF1C1B1F),
-    onSurface = Color(0xFF1C1B1F),
-    */
-)
 
 @Composable
 fun CatAPITheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
-    // Dynamic color is available on Android 12+
-    dynamicColor: Boolean = true,
     content: @Composable () -> Unit
 ) {
-    val colorScheme = when {
-        dynamicColor && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> {
-            val context = LocalContext.current
-            if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
-        }
+    val systemUiController = rememberSystemUiController()
+    val useDarkIcons = !darkTheme
 
-        darkTheme -> DarkColorScheme
-        else -> LightColorScheme
+    systemUiController.setStatusBarColor(
+        color = colorResource(id = if (darkTheme) R.color.darkBackgroundColor else R.color.backgroundColor),
+        darkIcons = useDarkIcons
+    )
+
+    systemUiController.setNavigationBarColor(
+        color = colorResource(id = if (darkTheme) R.color.darkBackgroundColor else R.color.backgroundColor),
+        darkIcons = useDarkIcons
+    )
+
+    val colorScheme = when {
+        darkTheme -> darkColorScheme(
+            primary = colorResource(id = R.color.primaryColor),
+            secondary = colorResource(id = R.color.secondaryColor),
+            background = colorResource(id = R.color.darkBackgroundColor),
+            surface = colorResource(id = R.color.darkSurfaceColor),
+            error = colorResource(id = R.color.errorColor),
+            onPrimary = colorResource(id = R.color.onPrimaryColor),
+            onError = colorResource(id = R.color.onErrorColor),
+            onBackground = colorResource(id = R.color.white),
+            onSurface = colorResource(id = R.color.white)
+        )
+
+        else -> lightColorScheme(
+            primary = colorResource(id = R.color.primaryColor),
+            secondary = colorResource(id = R.color.secondaryColor),
+            background = colorResource(id = R.color.backgroundColor),
+            surface = colorResource(id = R.color.surfaceColor),
+            error = colorResource(id = R.color.errorColor),
+            onPrimary = colorResource(id = R.color.onPrimaryColor),
+            onError = colorResource(id = R.color.onErrorColor),
+            onBackground = colorResource(id = R.color.onBackgroundColor),
+            onSurface = colorResource(id = R.color.onSurfaceColor)
+        )
     }
 
     MaterialTheme(
