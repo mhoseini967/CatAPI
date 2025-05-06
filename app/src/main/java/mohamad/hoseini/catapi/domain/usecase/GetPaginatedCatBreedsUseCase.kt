25443347aae1c17aa5eb6ba4eda69af1f369c0db
Mohamad Hoseini <mhoseini967@gmail.com>
2025-05-06 11:57:49 +0330

    add cat breed usecases

---
+++ app/src/main/java/mohamad/hoseini/catapi/domain/usecase/GetPaginatedCatBreedsUseCase.kt
@@ -0,0 +1,20 @@
+package mohamad.hoseini.catapi.domain.usecase
+
+import androidx.paging.PagingData
+import kotlinx.coroutines.flow.Flow
+import mohamad.hoseini.catapi.base.BaseUseCase
+import mohamad.hoseini.catapi.di.DispatcherProvider
+import mohamad.hoseini.catapi.domain.model.CatBreed
+import mohamad.hoseini.catapi.domain.repository.CatBreedRepository
+import javax.inject.Inject
+
+class GetPaginatedCatBreedsUseCase @Inject constructor(
+    dispatcherProvider: DispatcherProvider,
+    private val catBreedRepository: CatBreedRepository
+) :
+    BaseUseCase<Unit, Flow<PagingData<CatBreed>>>(dispatcherProvider) {
+    override suspend fun execute(params: Unit): Flow<PagingData<CatBreed>> {
+        return catBreedRepository.getCatBreedsPaged()
+    }
+
+}
